04e969cbbf43bd3f7f87838714e99ae24b3cf100
Your Name <you@example.com>
2025-01-28 09:54:56 +0000

    Edit ssh

--- terraform_infrastructure/cluster.tf
+++ terraform_infrastructure/cluster.tf
@@ -33,6 +33,7 @@ resource "yandex_compute_instance" "nat-instance" {
 
   metadata = {
     user-data = "${file("./meta.yml")}"
+    ssh-keys = "user:${var.ssh_public}"
     serial-port-enable = "1"
   }
 }
